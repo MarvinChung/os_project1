1e783ed88ef4ee02b25b530e4a0ac8f71bd52a7a
MarvinChung <marvin852316497@gmail.com>
2019-03-31 17:57:53 +0800

    Add files via upload

--- scheduler.h
+++ scheduler.h
@@ -10,15 +10,21 @@
 #define RR    2
 #define SJF   3
 #define PSJF  4
+#define MAXLENGTH 0xffffffffffffffff
 
 struct element{
 	int pid_index;
 	int t_exec;
 };
 
+struct element task_list[MAXLENGTH];
+
 int cmp(const void *a, const void *b);
 
-int next_process(struct process *proc, int nproc, int policy);
+struct element *FIFO_scheduling_table(struct process *proc, int nproc);
+struct element *RR_scheduling_table(struct process *proc, int nproc);
+struct element *SJF_scheduling_table(struct process *proc, int nproc);
+struct element *PSJF_scheduling_table(struct process *proc, int nproc);
 
 int scheduling(struct process *proc, int nproc, int policy);
 

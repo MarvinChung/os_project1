3d361b89f1a6b1aff051ae521a9c21586803fd73
vincentvbh <b05902122@ntu.edu.tw>
2019-03-31 22:13:20 +0800

    remove cmp and add basic scheduling protocols

--- scheduler.h
+++ scheduler.h
@@ -19,12 +19,10 @@ struct element{
 
 struct element task_list[MAXLENGTH];
 
-int cmp(const void *a, const void *b);
-
-struct element *FIFO_scheduling_table(struct process *proc, int nproc);
-struct element *RR_scheduling_table(struct process *proc, int nproc);
-struct element *SJF_scheduling_table(struct process *proc, int nproc);
-struct element *PSJF_scheduling_table(struct process *proc, int nproc);
+int FIFO_scheduling_table(struct process *proc, int nproc);
+int RR_scheduling_table(struct process *proc, int nproc);
+int SJF_scheduling_table(struct process *proc, int nproc);
+int PSJF_scheduling_table(struct process *proc, int nproc);
 
 int scheduling(struct process *proc, int nproc, int policy);
 

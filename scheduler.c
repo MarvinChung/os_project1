dfb7c0c797173db5466dc85e86a85ecf1a46b4c9
MarvinChung <marvin852316497@gmail.com>
2019-04-19 20:10:46 +0800

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -23,21 +23,35 @@ static int finish_cnt;
 
 int scheduling(struct process *proc, int nproc, int policy){
     int ntask;
+#ifdef DEBUG_FIFO
 	if(policy == FIFO)
 		ntask = FIFO_scheduling_table(proc, nproc);
+#endif
+#ifdef DEBUG_RR
 	if(policy == RR)
 		ntask = RR_scheduling_table(proc, nproc);
+#endif
+#ifdef DEBUG_SJF
 	if(policy == SJF)
 		ntask = SJF_scheduling_table(proc, nproc);
+#endif
+#ifdef DEBUG_PSJF
 	if(policy == PSJF)
 		ntask = PSJF_scheduling_table(proc, nproc);
-    
-    for(int i = 0; i < nproc; i++)
-    	proc[i].pid = -1;
+#endif
 
-    proc_assign_cpu(getpid(), PARENT_CPU);
+    for(int i = 0; i < ntask; i++){
+	printf("%s, time: %f \n", proc[task_list[i].pid_index].name
+	       , proc[task_list[i].t_exec]);
+    }
+	
+//    for(int i = 0; i < nproc; i++)
+//    	proc[i].pid = -1;
 
-    proc_wakeup(getpid());
+	
+//    proc_assign_cpu(getpid(), PARENT_CPU);
+
+//    proc_wakeup(getpid());
 
     ntime = 0;
     running = -1;
